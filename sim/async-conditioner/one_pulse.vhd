df2f59f5a738ef4d4adeb0182bb4ad95273b9a73
Ryan Dupuis <ryanstein813@gmail.com>
2024-09-23 16:57:22 -0600

    HW 6: One pulse passes tests

--- sim/async-conditioner/one_pulse.vhd
+++ sim/async-conditioner/one_pulse.vhd
@@ -16,10 +16,33 @@ end entity;
 
 architecture one_pulse_arch of one_pulse is
 	
-	
+	signal clock_cycles_on : natural := 0;
 	
 	begin
 		
-		
+		PULSE_GEN : process(rst, clk, input) is
+		begin
+			if rst = '1' then
+				-- Idle state, no pulse
+				pulse <= '0';
+				clock_cycles_on <= 0;
+			elsif rising_edge(clk) then
+				case (input) is
+					when '1' =>
+						-- Begin counting once input goes to 1
+						clock_cycles_on <= clock_cycles_on + 1;
+						-- Only drive pulse if count has just begun
+						if clock_cycles_on > 0 then
+							pulse <= '0';
+						else
+							pulse <= '1';
+						end if;
+					when others =>
+						-- Idle state, no pulse
+						pulse <= '0';
+						clock_cycles_on <= 0;
+				end case;
+			end if;
+		end process;
 		
 end architecture;
