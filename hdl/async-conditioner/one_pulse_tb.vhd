e01f3ab7e24a4a07b243b2de7acf061027a203d0
Ryan Dupuis <ryanstein813@gmail.com>
2024-09-25 22:06:03 -0600

    Hw 4: Lazy one pulse testbench

--- hdl/async-conditioner/one_pulse_tb.vhd
+++ hdl/async-conditioner/one_pulse_tb.vhd
@@ -10,6 +10,13 @@ end entity;
 
 architecture one_pulse_tb_arch of one_pulse_tb is
 	
+	constant CLK_PERIOD : time := 20 ns;
+	
+	signal clk_tb   : std_ulogic;
+	signal rst_tb   : std_ulogic;
+	signal input_tb : std_ulogic;
+	signal pulse_tb : std_ulogic;
+	
 	component one_pulse is
 		port (
 			clk   : in  std_ulogic;
@@ -25,13 +32,36 @@ architecture one_pulse_tb_arch of one_pulse_tb is
 			port map (
 				clk   => clk_tb,
 				rst   => rst_tb,
-				input => pulse
+				input => input_tb,
+				pulse => pulse_tb
 			);
 		
+		-- Clock generation
+		CLOCK_GEN : process is
+		begin
+			clk_tb <= '1'; wait for CLK_PERIOD;
+			clk_tb <= '0'; wait for CLK_PERIOD;
+		end process;
+		
+		-- DUT Stimulus
 		STIMULUS : process is
 		begin
 			
+			input_tb <= '0';
 			
+			-- Reset
+			rst_tb <= '0'; wait for CLK_PERIOD;
+			rst_tb <= '1'; wait for CLK_PERIOD * 2;
+			rst_tb <= '0';
+			
+			-- Asynchronous stimulus: strange input with quadraticly changing period
+			input_tb <= '0'; wait for CLK_PERIOD * 10;
+			for i in 0 to 15 loop
+				input_tb <= '1'; wait for CLK_PERIOD * i * i * 0.31415926535;
+				input_tb <= '0'; wait for CLK_PERIOD * i * i * 0.31415926535;
+			end loop;
+			
+			std.env.finish;
 			
 		end process;
 		
